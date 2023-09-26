b7cfc2bc62ae51ded8c20a6354ea77c8b3460645
Abele-n <otienoabele89@gmail.com>
2023-09-25 19:43:11 -0700

    1. Safe printing of an integers list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,7 +12,7 @@ def safe_print_list(my_list=[], x=0):
     naf = 0
     for j in range(x):
         try:
-            print("{}".format(my_list[j]), end = "")
+            print("{}".format(my_list[j]), end="")
             naf += 1
         except IndexError:
             break
