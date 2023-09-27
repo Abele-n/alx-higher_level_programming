f405d5677e64c206680a1760eca2604a88b65122
Abele-n <otienoabele89@gmail.com>
2023-09-27 02:17:46 -0700

    Infinite addition(python)

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     total_no = 0
     for m in range(len(sys.argv) - 1):
         total_no += int(sys.argv[m + 1])
-        print("{}".format(total_no))
+    print("{}".format(total_no))
