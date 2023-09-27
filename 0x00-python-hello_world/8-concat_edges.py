c781782c4368e2931741e42a4bbd2fece45b8885
Abele-n <otienoabele89@gmail.com>
2023-09-27 00:59:35 -0700

    8. Create a new sentence task.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str_x = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-my_str = str_x[39:67] + str_x[-22:-17] + str_x[:6]
+my_str = str_x[39:67] + str_x[107:112] + str_x[0:6]
 print(my_str)
