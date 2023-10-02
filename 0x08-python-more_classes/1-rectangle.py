330b87c6e8d05af5dcf816831869b72a72508557
Abele-n <otienoabele89@gmail.com>
2023-10-02 06:30:22 -0700

    Real definition of a rectangle task 1.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         @property
         def width(self):
             """returns the width of the rectangle"""
-            return self.__width
+            return self._width
 
         @width.setter
         def width(self, value):
@@ -25,12 +25,12 @@ class Rectangle:
                 raise TypeError("width must be an integer")
             if value < 0:
                 raise ValueError("width must be >= 0")
-            self.__width = value
+            self._width = value
 
         @property
         def height(self):
             """returns the height of the rectangle"""
-            return self.__height
+            return self._height
 
         @height.setter
         def height(self, value):
@@ -38,4 +38,4 @@ class Rectangle:
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
-            self.__height = value
+            self._height = value
