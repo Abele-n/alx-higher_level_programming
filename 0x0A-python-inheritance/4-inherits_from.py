f5fb25ea75bc42a8367fad1c0d7ec3eccd1ea83e
Abele-n <otienoabele89@gmail.com>
2023-10-11 09:37:20 -0700

    task 4 python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""inherits_from module"""
+"""inherits_from module definition"""
 
 
 def inherits_from(obj, a_class):
-    """is the object a true subclass of the class?"""
+    """is the object a true subclass of the class?
+
+    Args:
+        obj: object to check
+        a_class: specified class checking for inheritance
+    Returns:
+        True if the object is an instance of a class that inherited
+        Otherwise False
+    """
     return isinstance(obj, a_class) and type(obj) != class
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""Rectangle class module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeeometry
