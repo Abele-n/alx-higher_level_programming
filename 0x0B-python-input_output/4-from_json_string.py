2ce2e81135ce9716cb7b39e716a39bd6d33f7da0
Abele-n <otienoabele89@gmail.com>
2023-10-11 04:38:38 -0700

    task 4. From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,5 +4,10 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """return python object representation of a json string"""
-    return json.loads(my_str)
+    """return python object representation of a json string
+
+    Args:
+        my_obj(object): python object to be saved in json file
+        filename(str): name of file where object is saved
+    """
+    return json.loads(my_obj)
