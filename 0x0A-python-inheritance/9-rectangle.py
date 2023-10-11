f105b112d26ff37e5e8e4c0ecc4274491e57e714
Abele-n <otienoabele89@gmail.com>
2023-10-11 10:25:03 -0700

    task 9.py Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,10 +4,31 @@ BaseGeometry = __import__("7-base_geometry").BaseGeeometry
 
 
 class Rectangle(BaseGeometry):
-    """subclass representing a rectangle"""
+    """subclass representing a rectangle
+
+    Args:
+        width(int): width of the rectangle
+        height(int): height of the rectangle
+    """
     def __init__(self, width, height):
         """the constructor"""
         self.integer_validator("height", height)
         self.integer_validator("width", width)
         self.__height = height
         self.__width = width
+
+    def area(self):
+        """it calculatrs the area of the rectangle
+
+        Returns:
+            area(int): area of tje rectangle
+        """
+        return self.__height * self.__width
+    def __str__(self):
+        """
+        Returns the description of the rectangle
+
+        Returns:
+            str: the rectangle description
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
