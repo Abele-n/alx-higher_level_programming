39cee8d4bf515eef6dc88888145afee96d3a16be
Abele-n <otienoabele89@gmail.com>
2024-01-24 02:15:00 -0800

    catch me if you can, task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # scipt that causes the server to respond with You got me
-curl -s -L -X PUT "user_id=98" -H "origin: School" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
