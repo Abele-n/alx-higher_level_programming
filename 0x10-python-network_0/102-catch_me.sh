2ac57cfd46fddc4303a63e605cc07e76d97862ef
Abele-n <otienoabele89@gmail.com>
2024-01-24 02:10:40 -0800

    task 9, catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # scipt that causes the server to respond with You got me
-curl -s -X POST "$http://0.0.0.0:5000/catch_me" -d "user_id=98"
+curl -s -L -X PUT "user_id=98" -H "origin: School" 0.0.0.0:5000/catch_me
