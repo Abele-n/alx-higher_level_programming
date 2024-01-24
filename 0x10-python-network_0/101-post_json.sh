b9b53eb1c64b93b1c3ff64570d47b39de7b143f5
Abele-n <otienoabele89@gmail.com>
2024-01-24 01:42:22 -0800

    task 5, json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request
-curl -s -d @"$2" -H "Content-Type: application/ json" -X POST "$1"
+curl -s -d @"$2" -H "Content-Type: application/json" -X POST "$1"
