2ec622038d0db359afa15aa9b8640f9de22c2910
Abele-n <otienoabele89@gmail.com>
2024-01-26 23:09:09 -0800

    task 6 updated.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,8 +4,8 @@ script that takes in a URL and an email address, sends
 a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response
 """
-import sys
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
