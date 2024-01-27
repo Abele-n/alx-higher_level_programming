470b0e5a1dd23a79196c3bd9a927622f694faa1b
Abele-n <otienoabele89@gmail.com>
2024-01-27 02:07:43 -0800

    task 6 peak updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,7 @@ function that finds a peak in a list of unsorted integers.
 
 
 def is_it_peak(list_of_integers):
-    """checks peak in an unsorted integer"""
+    """is_it_peak: checks peak in an unsorted integer and None if empty"""
     li = list_of_integers
     ln = len(li)
     if ln == 0:
