691e092ffb6f86e1e9c2c87d900a5275f7376222
Abele-n <otienoabele89@gmail.com>
2024-01-26 22:53:36 -0800

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,7 +5,7 @@ a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response
 """
 import sys
-import requests
+import urllib.requests
 
 
 if __name__ == "__main__":
